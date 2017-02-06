95be1f4f7e7a10544347943c9e4e76e8318e2657
Matthew Little <matt@pingman.com>
2017-02-06 15:36:33 -0700

    Updated description

--- SsdpRadar.NET451/Properties/AssemblyInfo.cs
+++ SsdpRadar.NET451/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SsdpRadar")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SSDP (Simple Service Discovery Protocol) Discovery Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SsdpRadar")]
